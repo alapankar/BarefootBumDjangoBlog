4b1ad77f1cb613f9028f6ffabc15a70cced7bde7
Alapan Kar <alapankar27@gmail.com>
2020-04-14 08:52:08 +0530

    User Registration Application Installed (not connected yet to database)

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from users import views as user_views
+
 urlpatterns = [
     path('', views.home , name='blog-home'),
+    path('register/', user_views.register, name='blog-register'),
     path('about/', views.about , name='blog-about'),
 ]
