cdd50ea704c34221c60783ca6f99288cde87e7e4
Alapan Kar <alapankar27@gmail.com>
2020-04-18 10:37:02 +0530

    User profile model updated and crud functionality added

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home , name='blog-home'),
+    path('', PostListView.as_view() , name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view() , name='post-detail'),
+    path('post/new/', PostCreateView.as_view() , name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view() , name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view() , name='post-delete'),
     path('register/', user_views.register, name='blog-register'),
     path('profile/', user_views.profile, name='blog-profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='blog-login'),
--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username=form.cleaned_data.get('username')
             messages.success(request, f'Account created successfully for {username}')
-            return redirect('blog-home')
+            return redirect('blog-login')
     else:
         form=UserRegisterForm()
 
