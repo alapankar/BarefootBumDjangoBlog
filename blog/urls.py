2c69958670c118727519b372bcbabcd3314f8d14
Alapan Kar <alapankar27@gmail.com>
2020-04-21 09:22:04 +0530

    Password Reset feature added

--- blog/urls.py
+++ blog/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('profile/', user_views.profile, name='blog-profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='blog-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='blog-logout'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
     path('about/', views.about , name='blog-about'),
 ]
